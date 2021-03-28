313027920a7885dec232cd7d31b5f27b0b222194
Andriy Palyukha <apalyukha@gmail.com>
2021-03-28 18:43:47 +0300

    28.03.21 18:43

--- src/example_pydantic.py
+++ src/example_pydantic.py
@@ -1,23 +1,33 @@
-from pydantic import BaseModel
+from pydantic import BaseModel, ValidationError
+
+
+class Tag(BaseModel):
+    id: int
+    tag: str
 
 
 class City(BaseModel):
     city_id: int
     name: str
-    population: int
+    tags: list[Tag]
+    # population: int
 
 
 input_json = """
 {
-    "city_id" : 123,
-     "name" : "Lviv",
-      "population" : 1000000
-}
-"""
+    "city_id" : "79052",
+    "name" : "Lviv",
+    "tags" : [{
+        "id" : 1, "tag" : "capital"
+    },{
+        "id" : 2, "tag" : "big city"
+    }]
+} """
 
-# city = City.parse_raw(
-#     """{"city_id" : 123, "name" : "Lviv", "population" : 1000000}"""
-# )
-
-city = City.parse_raw(input_json)
-print(city)
+try:
+    city = City.parse_raw(input_json)
+except ValidationError as e:
+    print("Exception", e.json())
+else:
+    tag = city.tags[1]
+    print(tag.json())
